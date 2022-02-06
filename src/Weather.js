415722777de87da12ca86ee44c0fc5ac385b0774
HuongNguyen <92014413+HuongNguyen1989@users.noreply.github.com>
2022-02-06 16:56:00 +0700

    Add API integration and date

--- src/Weather.js
+++ src/Weather.js
@@ -1,56 +1,98 @@
-import React from "react";
+import React, { useState } from "react";
 import "./weather.css";
+import axios from "axios";
 
-function Weather() {
-  return (
-    <div className="container">
-      <div className="weather">
-        <form>
+function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  function handleResponse(response) {
+    const today = new Date(response.data.dt * 1000);
+    setWeatherData({
+      ready: true,
+      temperature: response.data.main.temp,
+      humidity: response.data.main.humidity,
+      wind: response.data.wind.speed,
+      description: response.data.weather[0].description,
+      icon: response.data.weather[0].icon,
+      date: today.toLocaleDateString("en-Us", {
+        weekday: "long",
+        hour: "2-digit",
+        minute: "2-digit",
+      }),
+      city: response.data.name,
+      country: response.data.sys.country,
+    });
+  }
+
+  function search() {
+    const apiKey = "7e62f7501b593a16608f7f0c6a1d755f";
+    const apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div className="container">
+        <div className="weather">
+          <form>
+            <div className="row">
+              <div className="col-9">
+                <input
+                  type="search"
+                  placeholder="Enter a city"
+                  className="form-control"
+                  autoFocus="on"
+                />
+              </div>
+              <div className="col-3">
+                <input
+                  type="submit"
+                  value="search"
+                  className="btn btn-primary"
+                />
+              </div>
+            </div>
+          </form>
           <div className="row">
-            <div className="col-9">
-              <input
-                type="search"
-                placeholder="Enter a city"
-                className="form-control"
+            <div className="col-6 today-weather">
+              <img
+                src="https://ssl.gstatic.com/onebox/weather/64/sunny_s_cloudy.png"
+                alt={weatherData.description}
               />
+              <span className="today-temperature">
+                {weatherData.temperature}
+              </span>
+              <span className="unit">°C</span>
+              <div className="today-parameter">
+                <ul>
+                  <li> Precipitation: precipitation% </li>
+                  <li> Humidity: {weatherData.humidity}% </li>
+                  <li> Wind: {weatherData.wind} mph </li>
+                </ul>
+              </div>
             </div>
-            <div className="col-3">
-              <input type="submit" value="search" className="btn btn-primary" />
-            </div>
-          </div>
-        </form>
-        <div className="row">
-          <div className="col-6 today-weather">
-            <img
-              src="https://ssl.gstatic.com/onebox/weather/64/sunny_s_cloudy.png"
-              alt="clear with perodic clouds"
-            />
-            <div className="today-temperature">21°C</div>
-            <div className="today-parameter">
-              <ul>
-                <li> Precipitation: 0% </li>
-                <li> Humidity: 53% </li>
-                <li> Wind: 5 mph </li>
+            <div className="col-6">
+              <h1>
+                {weatherData.city}, {weatherData.country}
+              </h1>
+              <ul className="today-description">
+                <li> {weatherData.date} </li>
+                <li>{weatherData.description} </li>
               </ul>
             </div>
           </div>
-          <div className="col-6">
-            <h1>New York, NY, USA</h1>
-            <ul className="today-description">
-              <li>Saturday 21:00 </li>
-              <li>Clear with periodic clounds </li>
-            </ul>
-          </div>
         </div>
+        <footer>
+          <p>
+            This project was coded by <a href="">Huong Nguyen </a>
+            and is <a href="">open-sourced on GitHub</a>
+          </p>
+        </footer>
       </div>
-      <footer>
-        <p>
-          This project was coded by <a href="">Huong Nguyen </a>
-          and is <a href="">open-sourced on GitHub</a>
-        </p>
-      </footer>
-    </div>
-  );
+    );
+  } else {
+    search();
+    return "Loading...";
+  }
 }
 
 export default Weather;
