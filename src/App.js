bef1d51c8a134bd6ade0428b3d1e3dd721f7ad30
HuongNguyen <92014413+HuongNguyen1989@users.noreply.github.com>
2022-02-06 08:44:09 +0700

    added footer-github link

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ function App() {
   return (
     <div className="App">
       <h1> Weather application</h1>
+      <footer>
+        <p>
+          This project was coded by <a href="">Huong Nguyen </a>
+          and is <a href="">open-sourced on GitHub</a>
+        </p>
+      </footer>
     </div>
   );
 }
