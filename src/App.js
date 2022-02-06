cdf0509488cf64ddb08399dc6e3fe3a5b484c8d5
HuongNguyen <92014413+HuongNguyen1989@users.noreply.github.com>
2022-02-06 12:45:46 +0700

    added weather HTML with CSS

--- src/App.js
+++ src/App.js
@@ -4,15 +4,7 @@ import Weather from "./Weather";
 function App() {
   return (
     <div className="App">
-      <div className="container">
-        <Weather />
-        <footer>
-          <p>
-            This project was coded by <a href="">Huong Nguyen </a>
-            and is <a href="">open-sourced on GitHub</a>
-          </p>
-        </footer>
-      </div>
+      <Weather />
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -3,42 +3,52 @@ import "./weather.css";
 
 function Weather() {
   return (
-    <div className="weather">
-      <form>
+    <div className="container">
+      <div className="weather">
+        <form>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a city"
+                className="form-control"
+              />
+            </div>
+            <div className="col-3">
+              <input type="submit" value="search" className="btn btn-primary" />
+            </div>
+          </div>
+        </form>
         <div className="row">
-          <div className="col-9">
-            <input
-              type="search"
-              placeholder="Enter a city"
-              className="form-control"
+          <div className="col-6 today-weather">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/sunny_s_cloudy.png"
+              alt="clear with perodic clouds"
             />
+            <div className="today-temperature">21°C</div>
+            <div className="today-parameter">
+              <ul>
+                <li> Precipitation: 0% </li>
+                <li> Humidity: 53% </li>
+                <li> Wind: 5 mph </li>
+              </ul>
+            </div>
           </div>
-          <div className="col-3">
-            <input type="submit" value="search" className="btn btn-primary" />
+          <div className="col-6">
+            <h1>New York, NY, USA</h1>
+            <ul className="today-description">
+              <li>Saturday 21:00 </li>
+              <li>Clear with periodic clounds </li>
+            </ul>
           </div>
-        </div>
-      </form>
-      <div className="row">
-        <div className="col-6">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/sunny_s_cloudy.png"
-            alt="clear with perodic clouds"
-          />
-          <h1>22oC</h1>
-          <ul>
-            <li> precipitation: 0% </li>
-            <li> Humidity: 53% </li>
-            <li> Wind: 5 mph </li>
-          </ul>
-        </div>
-        <div className="col-6">
-          <h1>New York, NY, USA</h1>
-          <ul>
-            <li>Saturday 21:00 </li>
-            <li>Clear with periodic clounds </li>
-          </ul>
         </div>
       </div>
+      <footer>
+        <p>
+          This project was coded by <a href="">Huong Nguyen </a>
+          and is <a href="">open-sourced on GitHub</a>
+        </p>
+      </footer>
     </div>
   );
 }
